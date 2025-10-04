7b36674069a1b0cd628f2ed94e60dcfb864cc78e
earthtraveller1 <liangyuezhao7@gmail.com>
2025-10-04 13:21:26 -0400

    Tried to remove the blacks (but failed).

--- API/api.py
+++ API/api.py
@@ -1,10 +1,26 @@
 from PIL import Image
 import os
-from datetime import date
+
 
 class PhytoplanktonImages:
-    def get_image(self, year, month, day) -> list[Image.Image]:
+    def get_image(self, year, month, day):
         print(f"{year}{month:02d}{day:02d}")
-        for line in open("nasa-hacks-2025/API/phytoplankton.txt", "r").readlines():
+        for line in open("API/phytoplankton.txt", "r").readlines():
             if f"{year}{month:02d}{day:02d}" in line:
                 os.system(f"curl --output ./{year}-{month}-{day}.png --url " + line)
+
+    def remove_blacks(self, image: Image.Image) -> Image.Image:
+        return Image.eval(
+            image, lambda color: 0.0 if color == 0x000000FF else color
+        )
+
+
+if __name__ == "__main__":
+    whatever = PhytoplanktonImages()
+    whatever.get_image(2025, 10, 3)
+    image = Image.open("2025-10-3.png")
+    image.convert("RGBA")
+    newImage = whatever.remove_blacks(image)
+    newImage.convert("RGBA")
+    # newImage.save("image.png")
+    newImage.show()
