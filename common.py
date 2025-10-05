c0671a5b12a96a1de83e2057ba6987471f5fff65
earthtraveller1 <liangyuezhao7@gmail.com>
2025-10-05 10:17:48 -0400

    Moved a lot of the stuff over to a common.py file.

---
+++ common.py
@@ -0,0 +1,53 @@
+import time
+
+SHARK_CATEGORIES = {
+    "White Shark (Carcharodon carcharias)": 1,
+    "Tiger Shark (Galeocerdo cuvier)": 0,
+    "Blacktip Shark (Carcharhinus limbatus)": 0,
+    "Shortfin Mako Shark (Isurus oxyrinchus)": 1,
+    "Blue Shark (Prionace glauca)": 1,
+    "Hammerhead Shark (Sphyrnidae)": 0,
+    "Silky Shark (Carcharhinus falciformis)": 0,
+    "Bull Shark (Carcharhinus leucas)": 0,
+    "Scalloped Hammerhead (Sphyrna lewini)": 0,
+    "Whale Shark (Rhincodon typus)": 0,
+    "Great Hammerhead (Sphyrna mokarran)": 0,
+    "Dusky Shark (Carcharhinus obscurus)": 0
+}
+
+
+class Shark:
+    def __init__(
+            self,
+            name: str,
+            id: int,
+            species: str,
+            location: tuple[float, float],
+            prev_location: tuple[float, float] = (0.0, 0.0),
+            predicted_location: tuple[float, float] = (0.0, 0.0),
+            mode: str = "",
+            prev_mode: str = "",
+            prev_mode_time: int = 0,
+            depth: float = 0.0,
+            prev_depth: float = 0.0,
+            px_x: int = 0,
+            px_y: int = 0,
+            facing: tuple[int, int] = (0, 0),
+    ):
+        self.name = name
+        self.id = id
+        self.species = species
+        self.location = location
+        self.predicted_location = predicted_location
+        self.mode = mode
+        self.prev_mode = prev_mode
+        self.prev_mode_time = time.time()
+        self.depth = depth
+        self.prev_depth = depth
+
+
+class TravelSpot:
+    def __init__(self, lat: float, long: float, date: str):
+        self.lat = lat
+        self.long = long
+        self.date = date
